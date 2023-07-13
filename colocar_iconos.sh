dfba963418451ac69a82b5230e6e29c6816fb75e
Manel <ea3eiz@ea3eiz.com>
2023-07-14 00:03:48 +0200

    Update colocar_iconos.sh

--- colocar_iconos.sh
+++ colocar_iconos.sh
@@ -1,18 +1,18 @@
 #!/bin/bash
-#Colores
-#ROJO="\033[1;31m"
-#VERDE="\033[1;32m"
-#BLANCO="\033[1;37m"
-#AMARILLO="\033[1;33m"
-#CIAN="\033[1;36m"
-#GRIS="\033[0m"
-#MARRON="\33[38;5;138m"
-#NEGRO="\33[0;30m"
-#clear
-#echo -n "${AMARILLO}"
-#echo "***********************************************************"
-#echo "         ESPERAR A QUE SE REORGANICEN LOS ICONOS          "
-#echo "***********************************************************"
+Colores
+ROJO="\033[1;31m"
+VERDE="\033[1;32m"
+BLANCO="\033[1;37m"
+AMARILLO="\033[1;33m"
+CIAN="\033[1;36m"
+GRIS="\033[0m"
+MARRON="\33[38;5;138m"
+NEGRO="\33[0;30m"
+clear
+echo -n "${AMARILLO}"
+echo "***********************************************************"
+echo "         ESPERAR A QUE SE REORGANICEN LOS ICONOS          "
+echo "***********************************************************"
 cd /home/pi/Desktop
 sudo rm *.desktop
 # recupera los iconos que se han borrado y se han convertido en icono de un libro 
