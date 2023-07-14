ee167e02f84be7b996d4490891d544262707f6a3
Manel <ea3eiz@ea3eiz.com>
2023-07-14 11:44:55 +0200

    Update copiar_restaurar_copiar.sh

--- copiar_restaurar_copiar.sh
+++ copiar_restaurar_copiar.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 clear
-            sudo rm /home/pi/.local/COPIA_SEGURIDAD/Downloads/*.*
+            sudo rm /home/pi/.local/COPIA_SEGURIDAD/Downloads/*
             
             cp /home/pi/.local/datos_dvswitch /home/pi/.local/COPIA_SEGURIDAD/Downloads
 
