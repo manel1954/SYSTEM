9a52f1c8922a1175b1423a67ccf99790e8380ed9
Manel <ea3eiz@ea3eiz.com>
2023-07-14 17:37:15 +0200

    Update actualizar_MMDVMHost.sh

--- actualizar_MMDVMHost.sh
+++ actualizar_MMDVMHost.sh
@@ -4,11 +4,12 @@
                     sudo rm -r /home/pi/MMDVMHost
                     sudo apt-get install build-essential git-core libi2c-dev i2c-tools lm-sensors
                     git clone https://github.com/g4klx/MMDVMHost
-                    #cd MMDVMHost
-                    #git clone https://github.com/hallard/ArduiPi_OLED
-                    #cd ArduiPi_OLED
-                    #sudo make clean
-                    #sudo make
+                    
+                    cd MMDVMHost
+                    git clone https://github.com/hallard/ArduiPi_OLED
+                    cd ArduiPi_OLED
+                    sudo make clean
+                    sudo make
                                     
                     cd /home/pi/MMDVMHost
 
@@ -22,8 +23,8 @@
 
                     make
                     sudo make install
-                    #make clean
-                    #make -f Makefile.Pi.OLED
+                    make clean
+                    make -f Makefile.Pi.OLED
 
                     #Instala la secion [NextionDriver] en todos los .ini y todas sus memorias
                     cd /home/pi/NextionDriverInstaller
