5d316c41b825d405785ad6a3eb039da06020b130
Manel <ea3eiz@ea3eiz.com>
2023-07-15 12:40:21 +0200

    Update colocar_iconos.sh

--- colocar_iconos.sh
+++ colocar_iconos.sh
@@ -22,7 +22,7 @@ sudo chmod 777 -R /home/pi/Desktop
 
 sudo cp /home/pi/icons.screen0-1904x1017.rc /home/pi/.config/xfce4/desktop
 sudo chmod 777 -R /home/pi/.config/xfce4/desktop
-xterm -geometry 87x6+653+136 -bg black -fg white -fa ‘verdana’ -fs 9x -T ESPERAR_A_QUE_SE_RECOLOQUEN_LOS_ICONOS -e xfdesktop --reload
+xfdesktop --reload
 
                            cd /home/pi/SYSTEM/qt/
                         ./qt_actualizando_ids                         
