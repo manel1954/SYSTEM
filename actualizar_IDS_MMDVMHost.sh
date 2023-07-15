5018ea19d4632ff4896c78f6c170a017c98a63c8
Manel <ea3eiz@ea3eiz.com>
2023-07-15 11:19:03 +0200

    Update actualizar_IDS_MMDVMHost.sh

--- actualizar_IDS_MMDVMHost.sh
+++ actualizar_IDS_MMDVMHost.sh
@@ -18,24 +18,24 @@ sleep 13
 cd /home/pi/SYSTEM/qt/
 ./qt_actualizando_imagen
 echo "${NEGRO}"
-var=`grep -n -m 1 '\<DMRIDPATH\>' /home/pi/MMDVMHost/linux/DMRIDUpdate.sh`
-buscar=":"
-largo_linea=`expr index $var $buscar`
-largo_linea=`expr $largo_linea - 1`
-numero_linea=`expr substr $var 1 $largo_linea`
-letrac=c
-linea_sed=$numero_linea$letrac
-sed -i "$linea_sed DMRIDPATH=/home/pi/MMDVMHost" /home/pi/MMDVMHost/linux/DMRIDUpdate.sh
-
-# DMR IDs now served by RadioID.net
-var=`grep -n -m 1 '\<DATABASEURL\>' /home/pi/MMDVMHost/linux/DMRIDUpdate.sh`
-buscar=":"
-largo_linea=`expr index $var $buscar`
-largo_linea=`expr $largo_linea - 1`
-numero_linea=`expr substr $var 1 $largo_linea`
-letrac=c
-linea_sed=$numero_linea$letrac
-sed -i "$linea_sed DATABASEURL='https://ham-digital.org/status/users.csv'" /home/pi/MMDVMHost/linux/DMRIDUpdate.sh
+#var=`grep -n -m 1 '\<DMRIDPATH\>' /home/pi/MMDVMHost/linux/DMRIDUpdate.sh`
+#buscar=":"
+#largo_linea=`expr index $var $buscar`
+#largo_linea=`expr $largo_linea - 1`
+#numero_linea=`expr substr $var 1 $largo_linea`
+#letrac=c
+#linea_sed=$numero_linea$letrac
+#sed -i "$linea_sed DMRIDPATH=/home/pi/MMDVMHost" /home/pi/MMDVMHost/linux/DMRIDUpdate.sh
+#
+## DMR IDs now served by RadioID.net
+#var=`grep -n -m 1 '\<DATABASEURL\>' /home/pi/MMDVMHost/linux/DMRIDUpdate.sh`
+#buscar=":"
+#largo_linea=`expr index $var $buscar`
+#largo_linea=`expr $largo_linea - 1`
+#numero_linea=`expr substr $var 1 $largo_linea`
+#letrac=c
+#linea_sed=$numero_linea$letrac
+#sed -i "$linea_sed DATABASEURL='https://ham-digital.org/status/users.csv'" /home/pi/MMDVMHost/linux/DMRIDUpdate.sh
 
 sudo sh /home/pi/MMDVMHost/linux/DMRIDUpdate.sh
 
