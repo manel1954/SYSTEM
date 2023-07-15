9be88335896bec269f3011f694ca061fcc7f74bd
Manel <ea3eiz@ea3eiz.com>
2023-07-15 10:48:44 +0200

    1

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -11,8 +11,9 @@ MARRON="\33[38;5;138m"
                         clear
                         echo "${GRIS}"
                         cd /home/pi/SYSTEM   
-                        xterm -geometry 87x6+652+138 -bg black -fg white -fa ‘verdana’ -fs 9x -T ACTUALIZANDO -e git pull                                      
-                        sh /home/pi/SYSTEM/ejecutar_ImagenActualizada.sh &
+                        git pull  
+                        /home/pi/SYSTEM/qt/ ./qt_actualizando_imagen                                    
+                        #sh /home/pi/SYSTEM/ejecutar_ImagenActualizada.sh &
                         mv qt_* /home/pi/SYSTEM/qt
                         chmod 777 -R /home/pi/SYSTEM/qt
 
