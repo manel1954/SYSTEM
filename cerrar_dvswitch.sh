12df8644560b59bc8134093abfea6419b3a72d93
Manel <ea3eiz@ea3eiz.com>
2023-08-06 19:15:20 +0200

    Update cerrar_dvswitch.sh

--- cerrar_dvswitch.sh
+++ cerrar_dvswitch.sh
@@ -9,7 +9,7 @@ sed -i "10c Name[es_ES]=Activar Dvswitch" /home/pi/Activar_dvswitch.desktop
 cd /home/pi
 sudo cp Activar_dvswitch.desktop /home/pi/Desktop
 sudo rm /home/pi/Activar_dvswitch.desktop
-
+sed -i "18c DVSWITCH=OFF" /home/pi/status.ini
 sudo systemctl stop ysfgateway.service
 sudo systemctl stop dmr2ysf.service
 sudo systemctl stop analog_bridge.service
@@ -22,7 +22,7 @@ sudo systemctl stop p25gateway.service
 
 
 #zenity --info --ok-label= --title=DVSWITCH --window-icon=/home/pi/SYSTEM/ICONO_DVSWITCH_OFF.png --width=400 --timeout=5 --text "\n\n\nDVSWITCH DESACTIVADO"
-sed -i "18c DVSWITCH=OFF" /home/pi/status.ini
+
 
 
 
