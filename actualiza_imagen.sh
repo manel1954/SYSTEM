15a8ecb80243c6da75e6f27f51fa7359a502b997
manel1954 <55663831+manel1954@users.noreply.github.com>
2023-11-19 19:39:15 +0100

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -8,12 +8,13 @@ AMARILLO="\033[1;33m"
 CIAN="\033[1;36m"
 GRIS="\033[0m"
 MARRON="\33[38;5;138m"
-                        #clear
-                        #echo "${GRIS}"
-                        #cd /home/pi/SYSTEM   
-                        #xterm -geometry 85x6+652+138 -bg black -fg white -fa ‘verdana’ -fs 9x -T ACTUALIZANDO -e git pull                                                              
-                        #mv qt_* /home/pi/SYSTEM/qt
-                        #chmod 777 -R /home/pi/SYSTEM/qt
+NEGRO="\e[30m"
+                        clear
+                        echo "${NEGRO}"
+                        cd /home/pi/SYSTEM   
+                        xterm -geometry 85x6+652+138 -bg black -fg white -fa ‘verdana’ -fs 9x -T ACTUALIZANDO -e git pull                                                              
+                        mv qt_* /home/pi/SYSTEM/qt
+                        chmod 777 -R /home/pi/SYSTEM/qt
                        
                         cd /home/pi/SYSTEM/qt/
                         ./qt_actualizando_imagen
