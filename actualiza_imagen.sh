0460e3a800335d681ff610432f1e6320f00f9775
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-16 20:35:23 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -11,18 +11,43 @@ MARRON="\33[38;5;138m"
 NEGRO="\e[30m"
                         clear
                         echo -n "${NEGRO}"
-                        cd /home/pi/SYSTEM   
-                        git pull --force                                                              
+                        
+                        #cd /home/pi/SYSTEM   
+                       # git pull --force                                                              
+                        #mv qt_* /home/pi/SYSTEM/qt
+                        #chmod 777 -R /home/pi/SYSTEM/                       
+                       # cd /home/pi/SYSTEM/qt/
+                       # ./qt_actualizando_imagen
+
+
+                        #cd /home/pi/SYSTEM   
+                       # git pull --force                                                              
+                        #mv qt_* /home/pi/SYSTEM/qt
+                        #chmod 777 -R /home/pi/SYSTEM/                       
+                       # cd /home/pi/SYSTEM/qt/
+                       # ./qt_actualizando_imagen
+                        
+                                        
+                        cd /home/pi/SYSTEM
+                        
+                        git pull --force
+
+                        
+
+                        sudo rm -R /home/pi/ORANGEPI
+
+                        mkdir /home/pi/ORANGEPI
+                                                
+                        cp -R /home/pi/SYSTEM/* /home/pi/ORANGEPI
+                        cp -R /home/pi/ORANGEPI/* /home/pi/SYSTEM
+                        cd /home/pi/SYSTEM
                         mv qt_* /home/pi/SYSTEM/qt
                         chmod 777 -R /home/pi/SYSTEM/                       
                         cd /home/pi/SYSTEM/qt/
                         ./qt_actualizando_imagen
+                      
                         
-
-
-
-
-
+                        #sudo rm -R /home/orangepi/qt
 
 
 
