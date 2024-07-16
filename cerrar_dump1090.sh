bf5b015e7336f4b1be9c40cf6b694e05d78152b9
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-16 19:21:24 +0200

    Update cerrar_dump1090.sh

--- cerrar_dump1090.sh
+++ cerrar_dump1090.sh
@@ -2,10 +2,9 @@
 
 sudo killall dump1090
 
-SCRIPTS_version=$(awk "NR==1" /home/pi/.config/autostart/version)
 cd /home/pi/Desktop
 sudo cp dump1090.desktop /home/pi
-sed -i "6c Exec=sh -c 'cd /home/pi/$SCRIPTS_version; sudo sh ejecutar_dump1090.sh'" /home/pi/dump1090.desktop
+sed -i "6c Exec=sh -c 'cd /home/pi/SYSTEM; sudo sh ejecutar_dump1090.sh'" /home/pi/dump1090.desktop
 sed -i "7c Icon=/home/pi/$SCRIPTS_version/ICO_AVION_OFF.png" /home/pi/dump1090.desktop
 sed -i "10c Name[es_ES]=Abrir Dump1090" /home/pi/dump1090.desktop
 
