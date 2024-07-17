80eeeda7300e2bf5a332e744b7e0e68bf5821c01
manel1954 <55663831+manel1954@users.noreply.github.com>
2024-07-17 13:02:47 +0200

    Update actualiza_imagen.sh

--- actualiza_imagen.sh
+++ actualiza_imagen.sh
@@ -14,10 +14,10 @@ NEGRO="\e[30m"
                         cd /home/pi/SYSTEM   
                         git pull                                                         
                         mv qt_* /home/pi/SYSTEM/qt
-                        #chmod 777 -R /home/pi/SYSTEM/                       
+                        chmod 777 -R /home/pi/SYSTEM/                       
                         cd /home/pi/SYSTEM/qt/
                         ./qt_actualizando_imagen
-                        
+
                         
 
 
